2d744b235b8d563cd8fdab863cc62a222b484d96
shroomlight <77877212@qq.com>
2025-01-14 14:10:32 +0800

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ in_class = False
 
 #遍历每一行代码
 for i in script :
-    if isnt_space.fun(i) :    
+    if isnt_space.fun(i) or i[:1] == '//':    
         #判断是否在解释一个类的声明
         if in_class :
             pass    #这里暂时先不写 （：3 」∠）
